15a0184712564ff9a58d9eb7708d30dd7568299c
jerryk1997 <>
2019-11-11 23:09:18 +0800

    no message

--- src/main/java/seedu/address/model/project/Title.java
+++ src/main/java/seedu/address/model/project/Title.java
@@ -29,19 +29,7 @@ public class Title {
         return test.matches(VALIDATION_REGEX);
     }
 
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
 
-        if (!(other instanceof Title)) {
-            return false;
-        }
-
-        Title otherTitle = (Title) other;
-        return otherTitle.title.equals(this.title);
-    }
     @Override
     public String toString() {
         return this.title;
